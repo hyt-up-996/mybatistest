f7e65310badf1c10e31a9d816bcbb217ce0f91da
何粤涛 <heyuetao@heyuetaodeMacBook-Pro.local>
2021-12-26 22:43:53 +0800

    init

---
+++ src/main/java/top/hyt/Demo.java
@@ -0,0 +1,11 @@
+package top.hyt;
+
+/**
+ * @author： Hyt
+ * @time： 2021-12-26 22:42
+ */
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("hello github");
+    }
+}
